040109f5bd12c4322b29e1238a3671446c449e18
yanchof <yoshihiroasrenchof@gmail.com>
2018-11-10 20:34:48 +0900

    エリート選択方式を実装

--- select.py
+++ select.py
@@ -1,12 +1,17 @@
 import random
-
+import numpy as np
 
 def roulette_wheel_selection(population, evaluation, selection_num):
     selected = random.choices(population=population, weights=evaluation, k=selection_num)
     return selected
 
-def rank_selection():
+
+def rank_selection(population, evaluation, selection_num):
     return
 
-def elite_selection():
-    return
+
+def elite_selection(population, evaluation, selection_num):
+    conbined = list(zip(population, evaluation))
+    list.sort(conbined, key=lambda x: x[1])
+    elite = [item[0] for item in conbined][:selection_num]
+    return elite
